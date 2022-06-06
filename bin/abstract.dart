6e2e30c4d4257ebe01fcb67f5943fa5a84ce6463
YossefHussein <youssef.hussein.business@gmail.com>
2022-06-06 20:51:29 +0200

    up

--- bin/abstract.dart
+++ bin/abstract.dart
@@ -1,15 +1,15 @@
 
 abstract class youssef {
- static var youssefNum1 = 5;
- static var youssefNum2 = 3;
+  var youssefNum1 = 5;
+  var youssefNum2 = 3;
   info({youssefNum1, youssefNum2}) {
-    print("مجموع ${youssefNum1 - youssefNum2}");
+    print(' ${youssefNum1 - youssefNum2}');
   }
 }
 
 class Hussein extends youssef {}
 
 main(List<String> args) {
-  Hussein hussein = Hussein();
-  hussein.info();
+  var hussein = Hussein();
+  hussein.info(youssefNum1: 5,youssefNum2: 8);
 }
